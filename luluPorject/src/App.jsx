15589aa958159ac7d8a96c439db3db1c2fd64c68
Desheng Kong <Kong033x@gmail.com>
2024-06-30 00:22:59 +1000

    footer

--- luluPorject/src/App.jsx
+++ luluPorject/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Header from './Component/Header.jsx'
 import Hero from './Component/Hero.jsx'
 import Body from './Component/Body.jsx'
+import Footer from './Component/Footer.jsx'
 const App = () => {
   return (
     <>
-    <div className='flex flex-col justify-center min-h-4 p-5'>
-    <Header/>
-    <Hero/>
-    <Body/>
+    <div>
+      <Header/>
+      <div className='flex flex-col justify-center min-h-4 p-5'>
+        <Hero/>
+        <Body/>
+      </div>
+      <Footer/>
     </div>
     </>
   )
---
+++ luluPorject/src/Component/Footer.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <div className='flex flex-row border border-solid justify-between p-5 border-gray-400 -mr-1'>
+        <ul className='flex flex-col space-y-2'>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs font-semibold inline-block relative">
+              CONTACT US
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Gift Cards
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Email Signup
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Stores
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+        </ul>
+
+        <ul className='flex flex-col space-y-2'>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs font-semibold inline-block relative">
+              HELP
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Gift Cards
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Email Signup
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Stores
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+        </ul>
+
+        <ul className='flex flex-col space-y-2'>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs font-semibold inline-block relative">
+              ABOUT US
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Gift Cards
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Email Signup
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Stores
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+        </ul>
+
+        <ul className='flex flex-col space-y-2'>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs font-semibold inline-block relative">
+              CONNECT WITH US
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Gift Cards
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Email Signup
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+          <li className='relative group'>
+            <a href="#" className="text-black text-xs inline-block relative">
+              Stores
+              <span className="absolute bottom-0 left-0 w-full h-0.5 bg-black bg-opacity-0 group-hover:bg-opacity-100 transition-opacity duration-300"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
